604df922712c8c32faa3e9027a91f5c8e24c6c11
BrianHo <Curry30@Brians-MacBook-Pro.local>
2018-09-11 17:04:45 -0700

    stage 2 - pokemon

--- angular/src/app/http.service.ts
+++ angular/src/app/http.service.ts
@@ -7,10 +7,25 @@ export class HttpService {
 
   constructor(private _http: HttpClient) { 
     this.getPokemon();
+    
   }
   getPokemon(){
-    let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
-    bulbasaur.subscribe(data => console.log("Get one pokemon", data))
-  }
-}
+    let pokemon = this._http.get('https://pokeapi.co/api/v2/pokemon/800/');
+    pokemon.subscribe(data => console.log("Get one pokemon", data))
+    pokemon.subscribe(data => {
+      for(var x in data.moves){
+        console.log("Get pokemon moves: " + data.moves[x].move["name"]);
+      }
+    })
+    pokemon.subscribe(data =>{console.log("similar ability pokemon");
+      let ability = this._http.get(data.abilities[0].ability.url);
+      ability.subscribe(data2 =>{console.log("similiar ability pokemons:");
+        for(var x in data2["pokemon"]){
+          console.log(data2["pokemon"][x]["pokemon"])
+        };
+
+      
+      })})} 
+  
+    }
 
